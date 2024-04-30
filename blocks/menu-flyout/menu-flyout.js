1585a0ac0253df05ad67f8710248672db9ff2e43
bharath-yathiraj <bharath.yathiraj@accenture.com>
2024-04-30 23:27:55 +0530

    menu flyout fixes

--- blocks/menu-flyout/menu-flyout.js
+++ blocks/menu-flyout/menu-flyout.js
@@ -1,56 +1,67 @@
-/*
-
 import { generateMenuTeaserDOM } from '../menu-teaser/menu-teaser.js';
 import { generateHeaderLinkList } from '../link-list/link-list.js';
 
-export function generateMenuFlyoutLink(props) {
+function generateMenuFlyoutLink(props) {
   const [menuflyoutText] = props;
-  const menuFlyoutLinkDOM = document.createRange().createContextualFragment(`
-    <span id="${menuflyoutText.textContent}"
- class="${menuflyoutText.textContent} menu-flyout-link">
-      ${menuflyoutText.textContent}
-    </span>
-  `);
-  return menuFlyoutLinkDOM;
+
+  // fetch menuflyout line text property value
+  const menuFlyourTitle = menuflyoutText.textContent;
+
+  // select p tag present in the props
+  const menuFlyoutDom = menuflyoutText.querySelector('p');
+
+  const spanTag = document.createElement('span');
+  spanTag.id = menuFlyourTitle;
+  spanTag.classList.add('menu-flyout-link');
+  spanTag.textContent = menuFlyourTitle;
+
+  menuFlyoutDom.textContent = '';
+  menuFlyoutDom.append(spanTag);
+
+  return menuFlyoutDom;
 }
 
 export default function decorate(block) {
+  const panelContainer = document.createElement('div');
+  panelContainer.classList.add('flyout-main-container');
+
+  // get all children elements
   const panels = [...block.children];
-  const menuProps = [...block.children].map((row) => row.firstElementChild);
-  const menuFlyoutLinkDOM = generateMenuFlyoutLink(menuProps);
-  const wrapperDiv = document.createElement('div');
-  wrapperDiv.classList.add('flyout-main-container');
+
+  // get menu fly out properties
+  const menuFlyoutProps = [...block.children].map((row) => row.firstElementChild);
+  const menuFlyoutDom = generateMenuFlyoutLink(menuFlyoutProps);
+
+  // loop through all children blocks
+  [...panels].forEach((panel) => {
+    // generate the  panel
+    const [, f2, , f4] = panel.children;
+
+    if (f2) {
+      const classesText = f2?.textContent.trim();
+      const classes = (classesText ? classesText.split(',') : []).map((c) => c?.trim()).filter((c) => !!c);
+
+      if ([...classes].includes('menu-teaser')) {
+        const props = [...panel.children].map((row) => row.firstElementChild);
+        panel.textContent = '';
+        panel.append(generateMenuTeaserDOM(props));
+        panelContainer.append(panel);
+      }
+    }
+
+    if (f4) {
+      const classesText = f4?.textContent.trim();
+      const classes = (classesText ? classesText.split(',') : []).map((c) => c?.trim()).filter((c) => !!c);
+
+      if ([...classes].includes('link-list')) {
+        const props = [...panel.children].map((row) => row.firstElementChild);
+        panel.textContent = '';
+        panel.append(generateHeaderLinkList(props));
+        panelContainer.append(panel);
+      }
+    }
+  });
   block.textContent = '';
-
-  // Append menu flyout link and panels to the wrapper div
-  block.appendChild(menuFlyoutLinkDOM);
-  [...panels].forEach((panel) => {
-    const [, , , field4] = panel.children;
-    const classesText = field4?.textContent.trim();
-    const classes = (classesText ? classesText.split(',') : [])
-      .map((c) => c && c.trim()).filter((c) => !!c);
-    if ([...classes].includes('link-list')) {
-      const props = [...panel.children].map((row) => row.firstElementChild);
-      panel.textContent = '';
-      wrapperDiv.appendChild(generateHeaderLinkList(props));
-    }
-  });
-  [...panels].forEach((panel) => {
-    const [, field2, ,] = panel.children;
-    const classesText = field2?.textContent.trim();
-    const classes = (classesText ? classesText.split(',') : [])
-      .map((c) => c && c.trim()).filter((c) => !!c);
-    if ([...classes].includes('menu-teaser')) {
-      const props = [...panel.children].map((row) => row.firstElementChild);
-      panel.textContent = '';
-      wrapperDiv.appendChild(generateMenuTeaserDOM(props));
-    }
-  });
-  const flyoutWrapperDiv = document.createElement('div');
-  flyoutWrapperDiv.classList.add('flyout-wrapper');
-
-  flyoutWrapperDiv.appendChild(wrapperDiv);
-  block.appendChild(flyoutWrapperDiv);
+  block.append(menuFlyoutDom);
+  block.append(panelContainer);
 }
-
-*/
