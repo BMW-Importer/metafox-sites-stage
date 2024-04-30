2598ab60164c3e16fefd3a427d6d882427c16ce9
bharath-yathiraj <bharath.yathiraj@accenture.com>
2024-04-30 17:12:08 +0530

    console error fixes in lint list js

--- blocks/link-list/link-list.js
+++ blocks/link-list/link-list.js
@@ -44,9 +44,9 @@ export function generateHeaderLinkList(props) {
       <div class="flyout-link-list">     
         <div class ="link-list-wrapper vertical">
           <div class="link-list block" data-block-name="link-list" data-block-status="loaded">
-            <h3 class="link-list-title">${b.textContent}</h3>
+            <h3 class="link-list-title">${b?.textContent}</h3>
               <div class="link-list-detail">
-               ${ulList.innerHTML}
+               ${ulList?.innerHTML}
               </div>
         </div>
       </div>
