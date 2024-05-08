2f9d6157f4a6ed72784b1ceac5f565c9f11fc325
Babita Sahu <babita.sahu@accenture.com>
2024-05-08 16:45:34 +0530

    revert some code

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -170,7 +170,6 @@ function getVideoElement(
       }
     });
   } else {
-    video.removeAttribute('playOnHover');
     video.removeEventListener('mouseenter', () => {});
     video.removeEventListener('mouseleave', () => {});
   }
