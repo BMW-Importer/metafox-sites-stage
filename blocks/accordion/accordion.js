3b3488139b6c2ccf2076e5a42164c1ea138f5981
Babita Sahu <babita.sahu@accenture.com>
2024-05-10 17:56:50 +0530

    completed the data reading for accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,34 +1,11 @@
-export function generateAccordionDOM(props) {
-  const { accordionItemLabel, CopyText, collapseAccordion } = props;
-
-  const accordionDOM = document.createRange().createContextualFragment(`
-    <div><div>${accordionItemLabel}</div></div>
-    <div><div>${CopyText}</div></div>
-    <div><div>${collapseAccordion}</div></div>
-
-  `);
-  return accordionDOM;
-}
 export default async function decorate(block) {
-  const props = [...block.children].map((row) => {
-    const [accordionItemLabel, CopyText, collapseAccordion] = Array.from(row.querySelectorAll('p')).map((p) => p?.textContent);
-    return { accordionItemLabel, CopyText, collapseAccordion };
+  const panels = [...block.children];
+  const accordionContainer = document.createElement('div');
+  accordionContainer.classList.add('accordion-container');
+  // loop through all children blocks
+  [...panels].forEach((panel) => {
+    const [accordionItem, CopyText, collapseAccordion] = panel.children;
+    console.log(accordionItem.textContent, CopyText.textContent, collapseAccordion.textContent);
   });
-
-  const accordionProps = props.reduce((acc, row) => {
-    Object.keys(row).forEach((key) => {
-      if (!acc[key]) {
-        acc[key] = '';
-      }
-      acc[key] += row[key];
-    });
-    return acc;
-  }, {});
-
-  const { accordionItemLabel, CopyText, collapseAccordion } = accordionProps;
-  console.log(accordionItemLabel, CopyText, collapseAccordion);
-
-  const accordionDOM = generateAccordionDOM(accordionProps);
   block.textContent = '';
-  block.append(accordionDOM);
 }
