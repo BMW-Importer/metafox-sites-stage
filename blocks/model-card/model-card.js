580feeb67c22ce1a3da9e1d951f95f4a3080c8b3
Aaditya Batra <aaditya.batra@accenture.com>
2024-05-14 02:51:42 +0530

    Update for wdh contextualization

--- blocks/model-card/model-card.js
+++ blocks/model-card/model-card.js
@@ -1,18 +1,29 @@
-/* async function buildGetPlaceholder(modelCode) {
+import { buildGetPlaceholder, getModelPlaceholderObject } from '../../scripts/common/wdh-placeholders.js';
+
+async function modelPlaceholder(modelCode) {
   try {
     const endpointUrl = `/WDH_API/Models/ModelDetails/${modelCode[0]}.json`;
     const response = await fetch(endpointUrl);
     const responseJson = await response.json();
-    console.log(responseJson);
+    buildGetPlaceholder(responseJson);
+    return getModelPlaceholderObject();
   } catch (error) {
     console.log('Error fetching data for building get placeholder', error);
+    throw error;
   }
 }
-*/
+
 export default function decorate(block) {
   const props = [...block.children].map((row) => row.firstElementChild);
-  const [contentfragment] = props;
-  console.log(contentfragment);
-  // const modelCode = ['7K11', '61FF'];
-  // buildGetPlaceholder(modelCode);
+  const [, placeholder] = props;
+  const wdhModelPlaceholder = placeholder.match(/\$\{model.\w*\}/g);
+  if (wdhModelPlaceholder) {
+    const modelCode = ['7K11', '61FF'];
+    let placeholderValue = '';
+    modelPlaceholder(modelCode).then((wdhPlaceholderObject) => {
+      placeholderValue = wdhPlaceholderObject[wdhModelPlaceholder[0]];
+      console.log('Placeholder Value', placeholderValue);
+    });
+  }
+  // console.log('Placeholder value', placeholderValue);
 }
