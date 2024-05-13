80bbb3136fbdd1e13f77b8e886378618543caa0f
Babita Sahu <babita.sahu@accenture.com>
2024-05-13 18:43:40 +0530

    Merge branch 'main' of https://github.com/BMW-Importer/metafox-sites into feature/metafox-101

---
+++ blocks/model-card/model-card.js
@@ -0,0 +1,18 @@
+/* async function buildGetPlaceholder(modelCode) {
+  try {
+    const endpointUrl = `/WDH_API/Models/ModelDetails/${modelCode[0]}.json`;
+    const response = await fetch(endpointUrl);
+    const responseJson = await response.json();
+    console.log(responseJson);
+  } catch (error) {
+    console.log('Error fetching data for building get placeholder', error);
+  }
+}
+*/
+export default function decorate(block) {
+  const props = [...block.children].map((row) => row.firstElementChild);
+  const [contentfragment] = props;
+  console.log(contentfragment);
+  // const modelCode = ['7K11', '61FF'];
+  // buildGetPlaceholder(modelCode);
+}
