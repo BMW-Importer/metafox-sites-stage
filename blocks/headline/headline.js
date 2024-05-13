99c1b7141392e066dd00bf2e14b13bbea4f0e196
Babita Sahu <babita.sahu@accenture.com>
2024-05-13 14:25:10 +0530

    added hidden class

--- blocks/headline/headline.js
+++
@@ -1,5 +0,0 @@
-export default function decorate(block) {
-  // get the first and only cell from each row
-  const props = [...block.children].map((row) => row.firstElementChild);
-  console.log(props);
-}
