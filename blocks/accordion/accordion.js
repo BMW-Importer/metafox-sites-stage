7ed0c0769538ea0e106da6450428bc319e5ccce7
Chaitanya <c.sravanthi.chappidi@accenture.com>
2024-05-14 18:57:12 +0530

    accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -13,13 +13,13 @@ export default function decorate(block) {
       summary.innerHTML = `<p>${summary.innerHTML}</p>`;
     }
     // decorate accordion item body
-    const body = row.children[1];
+    const body = row.children[2];
     body.className = 'accordion-item-body';
     if (!hasWrapper(body)) {
       body.innerHTML = `<p>${body.innerHTML}</p>`;
     }
 
-    const collapse = row.children[2].firstElementChild?.textContent.trim() === 'true';
+    const collapse = row.children[1].firstElementChild?.textContent.trim() === 'true';
     // decorate accordion item
     const details = document.createElement('details');
     details.className = 'accordion-item';
@@ -30,6 +30,7 @@ export default function decorate(block) {
     details.append(summary, body);
     row.textContent = '';
     row.append(details);
+    block.append(row);
     // row.replaceWith(details);
   });
 }
