1e01b1e31e59b75c75abfdb3a077aece38f09576
Chaitanya <c.sravanthi.chappidi@accenture.com>
2024-05-14 14:51:55 +0530

    accordion changes

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -7,11 +7,9 @@ export default function decorate(block) {
     // decorate accordion item label
     const label = row.children[0];
     const summary = document.createElement('summary');
-    summary.className = 'accordion-item-label';
     summary.append(...label.childNodes);
     // decorate accordion item body
     const body = row.children[1];
-    body.className = 'accordion-item-body';
     const collapse = row.children[2].firstElementChild?.textContent.trim() === 'true';
     // decorate accordion item
     const details = document.createElement('details');
@@ -21,8 +19,9 @@ export default function decorate(block) {
       details.setAttribute('open', '');
     }
     details.append(summary, body);
-    // details.setAttribute("open");
-    row.textContent = '';
-    row.append(details);
+    // row.textContent = '';
+    // row.append(details);
+    row.replaceWith(details);
+    // block.append(details);
   });
 }
