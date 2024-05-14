cbe57a41cb0fb6b11ce2caa17081129c7165aae7
Harish-DV <163002991+Harish-DV@users.noreply.github.com>
2024-05-14 11:30:02 +0200

    Update iframe.js

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,15 +1,15 @@
 export function generateIFrameDOM(props) {
   // Extract properties, always same order as in model, empty string if not set
   const [iFrameUrl] = props;
-  const iframeSrc = iFrameUrl.textContent;
+  let iframeSrc = iFrameUrl.textContent;
   console.log("iframe URL : "+iframeSrc);
-  const anchor =  alloy('appendIdentityToUrl',iFrameUrl.textContent);
-  console.log("anchor : "+anchor);
+  alloy("appendIdentityToUrl", {url: iframeSrc}).then(result=> {iframeSrc = result.url;});
+  console.log("anchor : "+iframeSrc);
 
   // Build DOM
   const iFrameDOM = document.createRange().createContextualFragment(`
       <div class="iframe-container">
-      <iframe src="${anchor}" id="bmwIframe" style="border: 0; width: 100%; height: 100%; min-height: 900px;">
+      <iframe src="${iframeSrc}" id="bmwIframe" style="border: 0; width: 100%; height: 100%; min-height: 900px;">
       </iframe>
       <div class="loader"></div>
        </div>
