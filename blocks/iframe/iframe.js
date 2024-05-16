eede7fcb657b59a673977e536234afa3ccc4516b
Harish-DV <163002991+Harish-DV@users.noreply.github.com>
2024-05-16 14:53:24 +0200

    Update iframe.js

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -12,7 +12,6 @@ export function generateIFrameDOM(props) {
   // Extract properties, always same order as in model, empty string if not set
   const [iFrameUrl] = props;
   const iframeSrc = iFrameUrl.textContent;
-  opt_in_info();
   console.log("iframe URL : "+iframeSrc);
   let anchor = iframeSrc;
   alloy("appendIdentityToUrl", {url: iframeSrc}).then(result=> {anchor = result.url;});
@@ -27,44 +26,6 @@ export function generateIFrameDOM(props) {
        </div>
     `);
   return iFrameDOM;
-}
-
-function opt_in_info(){
-  const dateTime = new Date();
-
-// Format the date components
-const year = dateTime.getFullYear();
-const month = (dateTime.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-const day = dateTime.getDate().toString().padStart(2, '0');
-const hours = dateTime.getHours().toString().padStart(2, '0');
-const minutes = dateTime.getMinutes().toString().padStart(2, '0');
-const seconds = dateTime.getSeconds().toString().padStart(2, '0');
-const milliseconds = dateTime.getMilliseconds().toString().padStart(3, '0');
-const timezoneOffset = -dateTime.getTimezoneOffset();
-const timezoneOffsetHours = Math.floor(Math.abs(timezoneOffset) / 60).toString().padStart(2, '0');
-const timezoneOffsetMinutes = (Math.abs(timezoneOffset) % 60).toString().padStart(2, '0');
-const timezoneSign = timezoneOffset >= 0 ? '+' : '-';
-
-// Construct the timestamp string
-const timestamp = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}${timezoneSign}${timezoneOffsetHours}:${timezoneOffsetMinutes}`;
-
-  const adobeDtm = window.adobeDataLayer;
-  console.log(adobeDtm.version);
-  const d = new Date();
-  alloy('setConsent', {
-    consent: [{
-      standard: 'Adobe',
-      version: '2.0',
-      value: {
-        collect: {
-          val: 'y'
-        },
-        metadata: {
-          time: timestamp
-        }
-      }
-    }]
-  });
 }
 
 export default function decorate(block) {
