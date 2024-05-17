3b5b36cdcffd2aadc87ab563f995f83d6daba2d6
Agnese Saulite <a.saulite@accenture.com>
2024-05-17 15:16:42 +0300

    added css & updated js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,25 @@
-export default function decorate(block) {
-  const [quoteWrapper] = block.children;
+export function generateQuoteDOM(props) {
+  // Extract properties, always same order as in model, empty string if not set
+  const [quote, source] = props;
+  // Build DOM
+  const quoteDOM = document.createElement('div');
 
-  const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
+  const quoteContainer = document.createElement('div');
+  quoteContainer.classList.add('quote');
+  quoteContainer.textContent = quote.textContent;
+  quoteDOM.append(quoteContainer);
+
+  const sourceContainer = document.createElement('div');
+  sourceContainer.classList.add('source');
+  sourceContainer.text = source.textContent;
+  quoteDOM.append(sourceContainer);
+
+  // Add final teaser DOM and classes if used as child component
+  return quoteDOM;
+}
+
+export default function decorate(block) {
+  const [quote, source] = block.children;
+  quote.className = 'quote';
+  source.className = 'source';
 }
