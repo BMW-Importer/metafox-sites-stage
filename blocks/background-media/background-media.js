13553b2700791386f5c6e421e117536d7d571057
bharath-yathiraj <bharath.yathiraj@accenture.com>
2024-06-10 10:10:06 +0530

    background media changes

--- blocks/background-media/background-media.js
+++ blocks/background-media/background-media.js
@@ -22,10 +22,13 @@ function generateCtaButtons(cta, btnContainer) {
 function generateTextProps(generalProps, generalPropIcon, copyTextContainer, gradientEffectClas) {
   // extracting eyebrow and headline
   let eyebrowText = generalProps.querySelectorAll('h4, h5, h6');
-  let headlineText = generalProps.querySelectorAll('h1, h2, h3');
+  let headlineText = generalProps.querySelector('p');
+  const headLineClass = generalProps.querySelector('h2');
 
   eyebrowText = eyebrowText.length > 0 ? eyebrowText[0] : '';
-  headlineText = headlineText.length > 0 ? headlineText[0] : '';
+
+  // appending heading class
+  if (headLineClass)  headLineClass?.classList?.add(headLineClass?.textContent || '');
 
   // extracting subrand icon
   const [subBrancdIcon] = generalPropIcon ? generalPropIcon.children : '';
