b36852d92f8ac42098cab0971200c38af6af0a5b
Dirk <drudolph@adobe.com>
2024-06-10 12:47:06 +0200

    fix: set opacity of icon font to 0 while loading

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -119,25 +119,25 @@ function launchVariables() {
 }
 
 
-function opt_in_info(){
+function opt_in_info() {
   const dateTime = new Date();
 
-// Format the date components
-const year = dateTime.getFullYear();
-const month = (dateTime.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-const day = dateTime.getDate().toString().padStart(2, '0');
-const hours = dateTime.getHours().toString().padStart(2, '0');
-const minutes = dateTime.getMinutes().toString().padStart(2, '0');
-const seconds = dateTime.getSeconds().toString().padStart(2, '0');
-const milliseconds = dateTime.getMilliseconds().toString().padStart(3, '0');
-const timezoneOffset = -dateTime.getTimezoneOffset();
-const timezoneOffsetHours = Math.floor(Math.abs(timezoneOffset) / 60).toString().padStart(2, '0');
-const timezoneOffsetMinutes = (Math.abs(timezoneOffset) % 60).toString().padStart(2, '0');
-const timezoneSign = timezoneOffset >= 0 ? '+' : '-';
+  // Format the date components
+  const year = dateTime.getFullYear();
+  const month = (dateTime.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+  const day = dateTime.getDate().toString().padStart(2, '0');
+  const hours = dateTime.getHours().toString().padStart(2, '0');
+  const minutes = dateTime.getMinutes().toString().padStart(2, '0');
+  const seconds = dateTime.getSeconds().toString().padStart(2, '0');
+  const milliseconds = dateTime.getMilliseconds().toString().padStart(3, '0');
+  const timezoneOffset = -dateTime.getTimezoneOffset();
+  const timezoneOffsetHours = Math.floor(Math.abs(timezoneOffset) / 60).toString().padStart(2, '0');
+  const timezoneOffsetMinutes = (Math.abs(timezoneOffset) % 60).toString().padStart(2, '0');
+  const timezoneSign = timezoneOffset >= 0 ? '+' : '-';
 
-// Construct the timestamp string
-const timestamp = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}${timezoneSign}${timezoneOffsetHours}:${timezoneOffsetMinutes}`;
-  console.log("Time of optin : "+timestamp);
+  // Construct the timestamp string
+  const timestamp = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}${timezoneSign}${timezoneOffsetHours}:${timezoneOffsetMinutes}`;
+  console.log("Time of optin : " + timestamp);
   const adobeDtm = window.adobeDataLayer;
   console.log(adobeDtm.version);
   const d = new Date();
@@ -197,3 +197,13 @@ async function loadPage() {
 }
 
 loadPage();
+
+document.fonts.addEventListener('loading', ({ target }) => {
+  [...target].filter(ff => ff.family === 'bmw_next_icons')
+    .forEach((ff) => {
+      ff.loaded.then(() => {
+        document.body.classList.add(`iconfont${ff.weight}-loaded`);
+        console.log(`Icon font ${ff.weight} loaded`);
+      });
+    });
+});
