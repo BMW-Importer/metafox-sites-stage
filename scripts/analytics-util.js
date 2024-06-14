1b9fbffe3fe159eb0e7acc64d96c5e303ee800f9
SnehAras <sneha.aras@accenture.com>
2024-06-14 12:39:24 +0530

    Push analytics attribute - event ID

--- scripts/analytics-util.js
+++ scripts/analytics-util.js
@@ -30,11 +30,17 @@ const event_tracking = {"event": {
 
 export function pushCustomLinkAnalyticData(prop) {  
     const [analyticsLabel, primaryCategory, subCategory, blockName, sectionName, linkURL] = prop;
+    
+    const randomNum = 100000 + Math.random() * 900000;
+    event_tracking.event.eventInfo.id = Math.floor(randomNum).toString();
+
     event_tracking.event.eventInfo.category.primaryCategory = primaryCategory;
     event_tracking.event.eventInfo.category.subCategory = subCategory;
     event_tracking.event.eventInfo.category.linkName = analyticsLabel;
     event_tracking.event.eventInfo.category.linkURL = linkURL;
+
     event_tracking.event.eventInfo.block.blockInfo.blockName = blockName;
+
     event_tracking.event.eventInfo.section.sectionInfo.sectionName = sectionName;
 
     window.adobeDataLayer.push(event_tracking);
