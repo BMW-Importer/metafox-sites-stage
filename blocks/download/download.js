102725228b6cebe4a091ebff6ff98e15cc8075ec
Soundrapandian <k.soundrapandian@accenture.com>
2024-06-26 19:03:11 +0530

    Download analytics values added

--- blocks/download/download.js
+++ blocks/download/download.js
@@ -1,4 +1,36 @@
 export default function decorate(block) {
   const anchorEle = block.querySelector('a');
-  if (anchorEle) anchorEle.target = '_blank';
+  if (anchorEle) {
+    const linkUrl = anchorEle.href.toLowerCase();
+    const fileExtension = getFileType(linkUrl);
+    const linkName = getFileName(linkUrl);
+    const { blockName }= anchorEle.closest('.block').dataset;
+    const sectionId = anchorEle.closest('.section').dataset.analyticsLabel;
+    anchorEle.target = '_blank';
+    anchorEle.dataset.analyticsCategory = 'Download';
+    anchorEle.dataset.analyticsCustomClick = 'true';
+    if(fileExtension) {
+      anchorEle.dataset.analyticsSubCategory = fileExtension;
+    }
+    if(blockName){
+      anchorEle.dataset.analyticsBlockName = blockName;
+    }
+    if(sectionId){
+      anchorEle.dataset.analyticsSectionId = sectionId
+    }
+    if(linkName){
+      anchorEle.dataset.analyticsLinkName = linkName;
+    }
+  }
+  function getFileType(url) {
+    const lastDotPosition = url.lastIndexOf('.');
+    if (lastDotPosition === -1) {
+        return '';
+    }
+    return url.substring(lastDotPosition + 1);
+  }
+  function getFileName(url) {
+    const lastSlashPosition = url.lastIndexOf('/');
+    return url.substring(lastSlashPosition + 1);
+  }
 }
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -69,7 +69,8 @@ function addAnalyticsCustomClickEvent() {
       const blockName = e.target.getAttribute('data-analytics-block-name');
       const sectionId = e.target.getAttribute('data-analytics-section-id');
       const linkURL = e.target.getAttribute('href');
-      pushCustomLinkAnalyticData([analyticsLabel, primaryCategory, subCategory, blockName, sectionId, linkURL]);      
+      const linkName = e.target.getAttribute('data-analytics-link-name');
+      pushCustomLinkAnalyticData([analyticsLabel, primaryCategory, subCategory, blockName, sectionId, linkURL, linkName]);      
     });
   });
 }
