2fe9980f03fbaf1044eb8aa4451dfe86ec4b0adf
Soundrapandian <k.soundrapandian@accenture.com>
2024-06-26 19:14:12 +0530

    download lint error fixes

--- blocks/download/download.js
+++ blocks/download/download.js
@@ -1,36 +1,36 @@
 export default function decorate(block) {
   const anchorEle = block.querySelector('a');
-  if (anchorEle) {
-    const linkUrl = anchorEle.href.toLowerCase();
-    const fileExtension = getFileType(linkUrl);
-    const linkName = getFileName(linkUrl);
-    const { blockName }= anchorEle.closest('.block').dataset;
-    const sectionId = anchorEle.closest('.section').dataset.analyticsLabel;
-    anchorEle.target = '_blank';
-    anchorEle.dataset.analyticsCategory = 'Download';
-    anchorEle.dataset.analyticsCustomClick = 'true';
-    if(fileExtension) {
-      anchorEle.dataset.analyticsSubCategory = fileExtension;
-    }
-    if(blockName){
-      anchorEle.dataset.analyticsBlockName = blockName;
-    }
-    if(sectionId){
-      anchorEle.dataset.analyticsSectionId = sectionId
-    }
-    if(linkName){
-      anchorEle.dataset.analyticsLinkName = linkName;
-    }
-  }
   function getFileType(url) {
     const lastDotPosition = url.lastIndexOf('.');
     if (lastDotPosition === -1) {
-        return '';
+      return '';
     }
     return url.substring(lastDotPosition + 1);
   }
   function getFileName(url) {
     const lastSlashPosition = url.lastIndexOf('/');
     return url.substring(lastSlashPosition + 1);
+  }
+  if (anchorEle) {
+    const linkUrl = anchorEle.href.toLowerCase();
+    const fileType = getFileType(linkUrl);
+    const linkName = getFileName(linkUrl);
+    const { blockName } = anchorEle.closest('.block').dataset;
+    const sectionId = anchorEle.closest('.section').dataset.analyticsLabel;
+    anchorEle.target = '_blank';
+    anchorEle.dataset.analyticsCategory = 'Download';
+    anchorEle.dataset.analyticsCustomClick = 'true';
+    if (fileType) {
+      anchorEle.dataset.analyticsSubCategory = fileType;
+    }
+    if (blockName) {
+      anchorEle.dataset.analyticsBlockName = blockName;
+    }
+    if (sectionId) {
+      anchorEle.dataset.analyticsSectionId = sectionId;
+    }
+    if (linkName) {
+      anchorEle.dataset.analyticsLinkName = linkName;
+    }
   }
 }
