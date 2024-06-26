2464b41d4381c369092a573ebb838b445ef5f2f5
bharath-yathiraj <bharath.yathiraj@accenture.com>
2024-06-26 14:33:57 +0530

    drivetrain related scriptchanges

--- blocks/drivetrain-switch/drivetrain-switch.js
+++ blocks/drivetrain-switch/drivetrain-switch.js
@@ -23,89 +23,7 @@ async function getContentFragmentData(disclaimerCFPath, gqlOrigin) {
   return response.json();
 }
 
-export default async function decorate(block) {
-  block.classList.add('drivetrain-switch-block');
-
-  const leftPanel = document.createElement('div');
-  leftPanel.classList.add('dts-left-panel');
-  const rightPanel = document.createElement('div');
-  rightPanel.classList.add('dts-right-panel');
-
-  const selectedModelDdlMob = document.createElement('button');
-  selectedModelDdlMob.classList.add('dts-selected-model-mob');
-
-  const leftPanelModelGrouping = document.createElement('ul');
-  leftPanelModelGrouping.classList.add('dts-model-grouping');
-
-  const rightPanelTitleAndImg = document.createElement('div');
-  rightPanelTitleAndImg.classList.add('dts-right-model-title');
-
-  const rightPanelTechDetail = document.createElement('div');
-  rightPanelTechDetail.classList.add('dts-right-tech-detail');
-
-  const techTable = document.createElement('table');
-  techTable.setAttribute('role', 'table');
-  rightPanelTechDetail.append(techTable);
-
-  const techTableData = document.createElement('tbody');
-  techTableData.setAttribute('role', 'rowgroup');
-  techTable.append(techTableData);
-
-  const [
-    fuelType,
-    detailCell,
-    disclaimerFragment,
-    technicalDetail1Cell,
-    technicalDetail2Cell,
-    ...rows
-  ] = [...block.children].map((row, index) => {
-    if (index < 5) {
-      return row;
-    }
-    return [...row.children];
-  });
-  // extracting detail cell
-  const labelSource = fuelType?.querySelector('h2');
-  block.removeChild(fuelType);
-
-  const activeModelTitle = detailCell?.querySelector('h3');
-  rightPanelTitleAndImg.append(activeModelTitle);
-  rightPanel.append(rightPanelTitleAndImg);
-
-  const modelDescp = detailCell?.querySelector('h4');
-  rightPanel.append(modelDescp);
-
-  // appending table below the description
-  rightPanel.append(rightPanelTechDetail);
-
-  const technicalLink = detailCell?.querySelector('a');
-  rightPanelTechDetail.append(technicalLink);
-
-  const popover = detailCell?.querySelector('h6');
-  // removing detailcell so that it wont appear in content tree
-  block.removeChild(detailCell);
-
-  // disclaimer fragment
-  const [disclaimerCF] = disclaimerFragment?.children || '';
-
-  if (hostName) {
-    const match = regExp.exec(hostName);
-    if (match) {
-      galOrigin = publishDomain;
-    }
-  }
-  await getContentFragmentData(disclaimerCF, galOrigin).then((response) => {
-    const cfData = response?.data;
-    if (cfData) {
-      const disclaimerHtml = cfData?.disclaimercfmodelByPath?.item?.disclaimer?.html;
-      const disclaimerContent = document.createElement('div');
-      disclaimerContent.className = 'disclaimer-content';
-      disclaimerContent.innerHTML = disclaimerHtml;
-      rightPanelTechDetail.append(disclaimerContent);
-      block.removeChild(disclaimerFragment);
-    }
-  });
-
+function generateTechnicalData1(technicalDetail1Cell, technicalDetail2Cell, techTableData) {
   // extracting technical data
   const tableRow1 = document.createElement('tr');
 
@@ -138,10 +56,9 @@ export default async function decorate(block) {
     `));
   }
   if (tableRow2.textContent) techTableData.append(tableRow2);
+}
 
-  // removing techdetail1 so that it wont appear in content tree
-  block.removeChild(technicalDetail1Cell);
-
+function generateTechnicalData2(technicalDetail2Cell, techTableData) {
   const tableRow3 = document.createElement('tr');
 
   const dataFactLabel3 = technicalDetail2Cell?.querySelector('h2');
@@ -173,44 +90,148 @@ export default async function decorate(block) {
     `));
   }
   if (tableRow4.textContent) techTableData.append(tableRow4);
+}
+
+function generateLeftPanelModelList(modelGroup, element, selectedModelDdlMob, analytics, block) {
+  const [modelCategory, modelLink, isSelected] = modelGroup.children;
+  const [analyticsLabel, BtnType, btnSubType] = analytics.children;
+  element.textContent = '';
+  if (isSelected?.textContent === 'true') {
+    element.append(
+      document.createRange().createContextualFragment(`
+                <span>${modelCategory.textContent}</span>
+                <span></span>`),
+    );
+    selectedModelDdlMob.textContent = modelCategory.textContent;
+  } else {
+    element.append(
+      document.createRange().createContextualFragment(`
+                <span>${modelCategory.textContent}</span>
+                <a href='${modelLink.textContent}' data-analytics-label='${analyticsLabel?.textContent?.trim() || ''}'
+                data-analytics-category='${BtnType?.textContent?.trim() || ''}'
+                data-analytics-subCategory='${btnSubType?.textContent?.trim() || ''}'
+                data-analytics-block-name='${block?.dataset?.blockName?.trim() || ''}'
+                data-analytics-section-id='${block?.closest('.section')?.dataset?.analyticsLabel || ''}'
+                data-analytics-custom-click='true'
+                ></a>`),
+    );
+  }
+}
+
+function bindAnalyticsValue(analytics, technicalLink, block) {
+  if (analytics) {
+    const [analyticsLabel, BtnType, btnSubType] = analytics.children;
+    if (technicalLink) {
+      technicalLink.dataset.analyticsLabel = analyticsLabel?.textContent?.trim() || '';
+      technicalLink.dataset.analyticsCategory = BtnType?.textContent?.trim() || '';
+      technicalLink.dataset.analyticsSubCategory = btnSubType?.textContent?.trim() || '';
+      technicalLink.dataset.analyticsCustomClick = 'true';
+      technicalLink.dataset.analyticsBlockName = block?.dataset?.blockName || '';
+      technicalLink.dataset.analyticsSectionId = block?.closest('.section')?.dataset?.analyticsLabel || '';
+    }
+  }
+}
+
+export default async function decorate(block) {
+  block.classList.add('drivetrain-switch-block');
+  const leftPanel = document.createElement('div');
+  leftPanel.classList.add('dts-left-panel');
+  const rightPanel = document.createElement('div');
+  rightPanel.classList.add('dts-right-panel');
+  const selectedModelDdlMob = document.createElement('button');
+  selectedModelDdlMob.classList.add('dts-selected-model-mob');
+  const leftPanelModelGrouping = document.createElement('ul');
+  leftPanelModelGrouping.classList.add('dts-model-grouping');
+  const rightPanelTitleAndImg = document.createElement('div');
+  rightPanelTitleAndImg.classList.add('dts-right-model-title');
+  const rightPanelTechDetail = document.createElement('div');
+  rightPanelTechDetail.classList.add('dts-right-tech-detail');
+  const techTable = document.createElement('table');
+  techTable.setAttribute('role', 'table');
+  rightPanelTechDetail.append(techTable);
+  const techTableData = document.createElement('tbody');
+  techTableData.setAttribute('role', 'rowgroup');
+  techTable.append(techTableData);
+  const [
+    fuelType,
+    detailCell,
+    disclaimerFragment,
+    technicalDetail1Cell,
+    technicalDetail2Cell,
+    ...rows
+  ] = [...block.children].map((row, index) => {
+    if (index < 5) {
+      return row;
+    }
+    return row;
+  });
+  // extracting detail cell
+  const selectedFuelType = fuelType?.querySelector('h2');
+  block.removeChild(fuelType);
+  const activeModelTitle = detailCell?.querySelector('h3');
+  rightPanelTitleAndImg.append(activeModelTitle);
+  rightPanel.append(rightPanelTitleAndImg);
+  const modelDescp = detailCell?.querySelector('h4');
+  rightPanel.append(modelDescp);
+  // appending table below the description
+  rightPanel.append(rightPanelTechDetail);
+  const technicalLink = detailCell?.querySelector('a');
+  rightPanelTechDetail.append(technicalLink);
+  const popover = detailCell?.querySelector('h6');
+  if (popover.textContent === 'true') rightPanelTechDetail.classList.add('enable-popover');
+  // removing detailcell so that it wont appear in content tree
+  block.removeChild(detailCell);
+  // disclaimer fragment
+  const [disclaimerCF] = disclaimerFragment?.children || '';
+  if (hostName) {
+    const match = regExp.exec(hostName);
+    if (match) {
+      galOrigin = publishDomain;
+    }
+  }
+  await getContentFragmentData(disclaimerCF, galOrigin).then((response) => {
+    const cfData = response?.data;
+    if (cfData) {
+      const disclaimerHtml = cfData?.disclaimercfmodelByPath?.item?.disclaimer?.html;
+      const disclaimerContent = document.createElement('div');
+      disclaimerContent.className = 'disclaimer-content';
+      disclaimerContent.innerHTML = disclaimerHtml;
+      rightPanelTechDetail.append(disclaimerContent);
+      block.removeChild(disclaimerFragment);
+    }
+  });
+
+  generateTechnicalData1(technicalDetail1Cell, techTableData);
+
+  // removing techdetail1 so that it wont appear in content tree
+  block.removeChild(technicalDetail1Cell);
+
+  generateTechnicalData2(technicalDetail2Cell, techTableData);
 
   // removing techdetail1 so that it wont appear in content tree
   block.removeChild(technicalDetail2Cell);
 
   // looping through children model card blocks
   rows.forEach((element) => {
-    const [modelGroup, context, analytics] = element;
+    const [modelGroup, context, analytics] = element?.children || '';
 
-    if (context) {
-      const [seriesRangeCode, enableTechData, transmissionType] = context.children;
-    }
+    // if (context) {
+    //   const [seriesRangeCode, enableTechData, transmissionType] = context.children;
+    // }
 
-    if (analytics) {
-      const [analyticsLabel, BtnType, btnSubType] = analytics.children;
-    }
+    bindAnalyticsValue(analytics, technicalLink, block);
 
-    if (modelGroup) {
-      const [modelCategory, modelLink, isSelected] = modelGroup.children;
-      element.textContent = '';
-      if (isSelected) {
-        element.append(
-          document.createRange().createContextualFragment(`
-                <span>${modelCategory.textContent}</span>
-                <span></span>`),
-        );
-        selectedModelDdlMob.textContent = modelCategory.textContent;
-      } else {
-        element.append(
-          document.createRange().createContextualFragment(`
-                <span>${modelCategory.textContent}</span>
-                <a href='${modelLink.textContent}'></a>`),
-        );
-      }
+    if (modelGroup.children) {
+      generateLeftPanelModelList(modelGroup, element, selectedModelDdlMob, analytics, block);
       const modelListItem = document.createElement('li');
       modelListItem.append(element);
+      analytics.classList.add(selectedFuelType?.textContent || '');
       leftPanelModelGrouping.append(modelListItem);
     }
   });
+
+  // appending model group to UI
+  leftPanel.append(leftPanelModelGrouping);
 
   block.textContent = '';
   block.append(leftPanel);
