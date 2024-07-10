2cc422fc92271fb20f6f6f918f5546447ed43bd2
bharath-yathiraj <bharath.yathiraj@accenture.com>
2024-07-10 15:44:49 +0530

    saving changes

--- blocks/technical-data/technical-data.js
+++ blocks/technical-data/technical-data.js
@@ -1,63 +1,90 @@
-// import { getApiResponse } from '../../scripts/common/wdh-util.js';
+import { getApiResponse } from '../../scripts/common/wdh-util.js';
+const ddlModelTitle = 'Izaberite model';
+const ddlTransmissionTypeTitle = '';
 
-// export default async function decorate(block) {
-//     block.classList.add('technical-data-block');
-//     const [
-//         techDataProp,
-//         ...rows
-//     ] = [...block.children].map((row, index) => {
-//         if (index < 1) {
-//             return row;
-//         }
-//         return row;
-//     });
+function generateModelsDdl(listOfModels, dropDownContainer) {
+    const modelDdlContainer = document.createElement('div');
+    modelDdlContainer.classList.add('techdata-model-ddl-container');
 
-//     const heading = document.createElement('h2');
-//     heading.classList.add('techdata-selected-model-title');
-//     block.append(heading);
+    const modelHeadingSpan = document.createElement('span');
+    modelHeadingSpan.classList.add('techdata-model-ddl-title');
+    modelHeadingSpan.textContent = ddlModelTitle;
+    modelDdlContainer.append(modelHeadingSpan);
 
-//     const dropDownContainer = document.createElement('div');
-//     dropDownContainer.classList.add('techdata-ddl-container');
-//     block.append(dropDownContainer);
+    const modelDdlList = document.createElement('ul');
+    modelDdlList.classList.add('techdata-model-ddl-ul');
+    modelDdlContainer.append(modelDdlList);
 
-//     const enableAutoData = techDataProp?.querySelector('h2');
-//     const enableAccordion = techDataProp?.querySelector('h3');
-//     if (enableAccordion) block.classList.append('techdata-enable-accordion');
+    listOfModels.forEach((model) => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('techdata-model-ddl-li');
+        listItem.textContent = model?.json?.description;
+        modelDdlList.append(listItem);
+    });
+    dropDownContainer.append(modelDdlContainer);
+}
 
-//     const spreadSheetLink = techDataProp?.querySelector('a');
+function generateTechDataUI(techDataObj) {
 
-//     const listOfModels = [];
+}
 
-//     rows.forEach(async (modelData) => {
-//         const [modelProp, analyticsProp] = modelData?.children || [];
+export default async function decorate(block) {
+    block.classList.add('technical-data-block');
+    const [
+        techDataProp,
+        ...rows
+    ] = [...block.children].map((row, index) => {
+        if (index < 1) {
+            return row;
+        }
+        return row;
+    });
 
-//         modelProp.classList.append('techdata-model-ddl');
+    const heading = document.createElement('h2');
+    heading.classList.add('techdata-selected-model-title');
+    block.append(heading);
 
-//         const [modelPropData] = modelProp?.children || [];
-//         const splittedModelData = modelPropData?.textContent?.split(',');
+    const dropDownContainer = document.createElement('div');
+    dropDownContainer.classList.add('techdata-ddl-container');
+    block.append(dropDownContainer);
 
-//         if (splittedModelData && splittedModelData?.length >= 3) {
-//             const agCode = splittedModelData[2]?.trim() || '';
-//             try {
-//                 // wdh call or else spreadsheet call
-//                 if (enableAutoData) {
-//                     const modelDetailResponse = await getApiResponse(agCode);
-//                     listOfModels.push({
-//                         agCode: agCode,
-//                         analytics: analyticsProp,
-//                         json: modelDetailResponse,
-//                     });
-//                 }
-//             } catch (error) {
-//                 console.error('fetch model detail failed');
-//             }
-//         }
+    const enableAutoData = techDataProp?.querySelector('h2');
+    const enableAccordion = techDataProp?.querySelector('h3');
+    if (enableAccordion) block.classList.append('techdata-enable-accordion');
 
-//     });
+    const spreadSheetLink = techDataProp?.querySelector('a');
 
-//     if (listOfModels.length > 1) {
-//         generateModelsDdl(listOfModels);
-//     } else {
-//         generateTechDataUI(listOfModels[0]);
-//     }
-// }
+    const listOfModels = [];
+
+    rows.forEach(async (modelData) => {
+        const [modelProp, analyticsProp] = modelData?.children || [];
+
+        modelProp.classList.append('techdata-model-ddl');
+
+        const [modelPropData] = modelProp?.children || [];
+        const splittedModelData = modelPropData?.textContent?.split(',');
+
+        if (splittedModelData && splittedModelData?.length >= 3) {
+            const agCode = splittedModelData[2]?.trim() || '';
+            try {
+                // wdh call or else spreadsheet call
+                if (enableAutoData) {
+                    const modelDetailResponse = await getApiResponse(agCode);
+                    listOfModels.push({
+                        agCode: agCode,
+                        analytics: analyticsProp,
+                        json: modelDetailResponse,
+                    });
+                }
+            } catch (error) {
+                console.error('fetch model detail failed');
+            }
+        }
+    });
+
+    if (listOfModels.length > 1) {
+        generateModelsDdl(listOfModels, dropDownContainer);
+    } else {
+        generateTechDataUI(listOfModels[0]);
+    }
+}
