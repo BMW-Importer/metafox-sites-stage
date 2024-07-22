c9fe38a2cc55dbac366d520528a545fade0b8043
Babita Sahu <babita.sahu@accenture.com>
2024-07-23 03:41:28 +0530

    fixedlint issue

--- blocks/stock-locator-model-detail-definition-specification/stock-locator-model-detail-definition-specification.js
+++ blocks/stock-locator-model-detail-definition-specification/stock-locator-model-detail-definition-specification.js
@@ -1,14 +1,7 @@
 export default async function decorate(block) {
   const modelDetails = [...block.children];
-  modelDetails.forEach(modelDetail => {
+  modelDetails.forEach((modelDetail) => {
     const [general, analytics] = modelDetail.children;
-    let data = general.querySelectorAll('p');
-    const rfoButtonLabel = data[0] || '';
-    const rfoButtonSty = data[1] || '';
-    const dealerLocatioBtn = data[2] || '';
-    const dealerLocatioBtnSty = data[3] || '';
-    const infoIconTxt = data[4] || '';
-    const fallBckImgOpt = data[5] || '';
-    const disclaimer = data[6] || '';
-});
-};
+    console.log(general, analytics);
+  });
+}
--- blocks/stock-locator-model-overview-properties/stock-locator-model-overview-properties.js
+++ blocks/stock-locator-model-overview-properties/stock-locator-model-overview-properties.js
@@ -1,13 +1,7 @@
 export default async function decorate(block) {
   const modelOverview = [...block.children];
-  modelOverview.forEach(overview => {
-    const [general, content] = overview?.children;
-    // const modelTitleBtn
-    // const modelTitleBtnSty;
-    // const infoIconTxt
-    // const resultCounts
-    // const disclaimer
-    // const fallbckBannrContent
-    // console.log(modelTitleBtn);
-});
-};
+  modelOverview.forEach((overview) => {
+    const [general, content] = overview.children;
+    console.log(general, content);
+  });
+}
