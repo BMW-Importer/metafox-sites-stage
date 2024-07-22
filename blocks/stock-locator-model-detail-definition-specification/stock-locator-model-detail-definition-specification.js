26523bd5f389a066d405b248a091bbafa582e852
Babita Sahu <babita.sahu@accenture.com>
2024-07-23 03:33:03 +0530

    modify model

--- blocks/stock-locator-model-detail-definition-specification/stock-locator-model-detail-definition-specification.js
+++ blocks/stock-locator-model-detail-definition-specification/stock-locator-model-detail-definition-specification.js
@@ -1,3 +1,14 @@
 export default async function decorate(block) {
-  console.log(block);
-}
+  const modelDetails = [...block.children];
+  modelDetails.forEach(modelDetail => {
+    const [general, analytics] = modelDetail.children;
+    let data = general.querySelectorAll('p');
+    const rfoButtonLabel = data[0] || '';
+    const rfoButtonSty = data[1] || '';
+    const dealerLocatioBtn = data[2] || '';
+    const dealerLocatioBtnSty = data[3] || '';
+    const infoIconTxt = data[4] || '';
+    const fallBckImgOpt = data[5] || '';
+    const disclaimer = data[6] || '';
+});
+};
--- blocks/stock-locator-model-overview-properties/stock-locator-model-overview-properties.js
+++ blocks/stock-locator-model-overview-properties/stock-locator-model-overview-properties.js
@@ -1,3 +1,13 @@
 export default async function decorate(block) {
-  console.log(block);
-}
+  const modelOverview = [...block.children];
+  modelOverview.forEach(overview => {
+    const [general, content] = overview?.children;
+    // const modelTitleBtn
+    // const modelTitleBtnSty;
+    // const infoIconTxt
+    // const resultCounts
+    // const disclaimer
+    // const fallbckBannrContent
+    // console.log(modelTitleBtn);
+});
+};
